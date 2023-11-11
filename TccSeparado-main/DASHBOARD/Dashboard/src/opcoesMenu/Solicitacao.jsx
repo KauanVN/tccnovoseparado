66f1ad1c41011d21cc8951bb978409b778f91ad6
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2023-11-11 19:13:41 -0300

    adicionado mensagem de email, aprovação e reprovação

--- TccSeparado-main/DASHBOARD/Dashboard/src/opcoesMenu/Solicitacao.jsx
+++ TccSeparado-main/DASHBOARD/Dashboard/src/opcoesMenu/Solicitacao.jsx
@@ -1,30 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { useDisclosure, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
-import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
 import ModalCompSolicitacao from "./ModalCompSolicitacao";
-import ModalComp from "./ModalCompParque";
 import "../css/HomeAdm.css";
 import Header from "../Header";
 import Sidebar from "../Sidebar";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
-import emailjs from "emailjs-com";
+import emailjs from 'emailjs-com';
 
 function Solicitacao({ data, handleEditSolicitacao, handleDeleteSolicitacao }) {
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
-  const [dataEdit, setDataEdit] = useState({});
-  const [dataParque, setDataParque] = useState([]);
-  const [showParque, setShowParque] = useState(true);
-  const [showUsuario, setShowUsuario] = useState(false);
-  const [showSolicitacao, setShowSolicitacao] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [dados, setDados] = useState([]);
   var administrador = JSON.parse(localStorage.getItem("administrador"));
 
-  // Configurações do EmailJS (substitua com suas informações)
-  const emailjsConfig = {
-    serviceId: "gmailMessage",
-    templateId: "template_9vwh41m",
-    userId: "uyCZcUA5NodsSYL-X", // Substitua com seu user ID do EmailJS
+  // Função para enviar e-mail de aprovação
+  const sendEmailAprovacao = (toEmail) => {
+    const templateParams = {
+      to_email: toEmail,
+      subject: "Solicitação Aprovada",
+      message: "Sua solicitação foi aprovada. Você agora é um administrador!",
+    };
+
+    // Substitua 'SEU_SERVICE_ID', 'SEU_TEMPLATE_ID' e 'SEU_USER_ID' pelos seus dados do EmailJS
+    emailjs.send('service_02nhlch', 'template_sblgj1u', templateParams, 'cXjdFwYdSBazrrS7q')
+      .then((response) => {
+        console.log('E-mail enviado com sucesso:', response);
+      })
+      .catch((error) => {
+        console.error('Erro ao enviar e-mail:', error);
+      });
+  };
+
+  // Função para recusar (pode chamar esta função ao clicar em Recusar)
+  const recusarSolicitacao = (toEmail) => {
+    const templateParams = {
+      to_email: toEmail,
+      subject: "Solicitação Recusada",
+      message: "Sua solicitação foi recusada. Infelizmente, não foi possível aprovar neste momento.",
+    };
+
+    // Substitua 'SEU_SERVICE_ID', 'SEU_TEMPLATE_ID' e 'SEU_USER_ID' pelos seus dados do EmailJS
+    emailjs.send('service_02nhlch', 'template_sblgj1u', templateParams, 'cXjdFwYdSBazrrS7q')
+      .then((response) => {
+        console.log('E-mail enviado com sucesso:', response);
+      })
+      .catch((error) => {
+        console.error('Erro ao enviar e-mail:', error);
+      });
   };
 
   async function adicionarAdm(dadosAdm) {
@@ -53,92 +75,13 @@ function Solicitacao({ data, handleEditSolicitacao, handleDeleteSolicitacao }) {
           alert("administrador adicionado com sucesso");
 
           // Enviar e-mail de aprovação
-          enviarEmailAprovacao(dadosAdm.email);
-          atualizaStatus(dadosAdm);
+          sendEmailAprovacao(dadosAdm.email);
+          // Atualizar o status aqui, se necessário
         }
       }
     } catch (error) {
       console.error("Erro ao fazer a solicitação:", error);
     }
-  }
-
-  async function enviarEmailAprovacao(email) {
-    const templateParams = {
-      to_email: email,
-      message: "Sua solicitação foi recusada.",
-    };
-
-    emailjs
-      .send(
-        emailjsConfig.serviceId,
-        emailjsConfig.templateId,
-        templateParams,
-        emailjsConfig.userId
-      )
-      .then((response) => {
-        console.log("E-mail enviado com sucesso:", response);
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar e-mail:", error);
-      });
-  }
-
-  async function recusarSolicitacao(dadosAdm) {
-    try {
-      const token = await administrador.token;
-      if (token) {
-        const headers = {
-          "Content-type": "application/json; charset=UTF-8",
-          Authorization: `Bearer ${token}`,
-        };
-
-        const response = await fetch(
-          "https://tcc-production-e100.up.railway.app/api/solicitacoes",
-          {
-            method: "PUT",
-            headers: headers,
-            body: JSON.stringify({
-              idSolicitacoes: dadosAdm.idSolicitacoes,
-              email: dadosAdm.email,
-              senha: dadosAdm.senha,
-              status: 2,
-              lazer: {
-                idLazer: dadosAdm.lazer.idLazer,
-              },
-            }),
-          }
-        );
-
-        if (response.status === 200) {
-          alert("Solicitação recusada com sucesso");
-          enviarEmailRecusa(dadosAdm.email);
-          window.location.reload();
-        }
-      }
-    } catch (error) {
-      console.error("Erro ao fazer a solicitação:", error);
-    }
-  }
-
-  async function enviarEmailRecusa(email) {
-    const templateParams = {
-      to_email: email,
-      message: "Sua solicitação foi recusada.",
-    };
-
-    emailjs
-      .send(
-        emailjsConfig.serviceId,
-        emailjsConfig.templateId,
-        templateParams,
-        emailjsConfig.userId
-      )
-      .then((response) => {
-        console.log("E-mail de recusa enviado com sucesso:", response);
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar e-mail de recusa:", error);
-      });
   }
 
   async function buscarSolicitacao() {
@@ -175,7 +118,7 @@ function Solicitacao({ data, handleEditSolicitacao, handleDeleteSolicitacao }) {
 
   const OpenSidebar = () => {
     setOpenSidebarToggle(!openSidebarToggle);
-  }
+  };
 
   return (
     <div className="grid-container">
@@ -218,13 +161,13 @@ function Solicitacao({ data, handleEditSolicitacao, handleDeleteSolicitacao }) {
                   <Td>
                     <CheckIcon
                       fontSize={20}
-                      onClick={() => adicionarAdm(item)}
+                      onClick={() => sendEmailAprovacao(item.email)}
                     />
                   </Td>
                   <Td p={0}>
                     <CloseIcon
                       fontSize={20}
-                      onClick={() => recusarSolicitacao(item)}
+                      onClick={() => recusarSolicitacao(item.email)}
                     />
                   </Td>
                 </Tr>
